07a7950de7ac4f579153569e5802d19a788e9220
Manapyzz <alexandreb.picard@gmail.com>
2022-01-18 11:04:58 +0100

    add poo exersises

--- exersices/exo13/insertArticle.php
+++ exersices/exo13/insertArticle.php
@@ -1,22 +1,9 @@
 <?php
 
-// Connexion a la base de donnée
-$dsn = 'mysql:host=database;dbname=blog;charset=UTF8';
-
-try {
-    $pdo = new PDO($dsn, "root", "tiger", [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::MYSQL_ATTR_FOUND_ROWS => true
-        ]
-    );
-} catch (PDOException $exception) {
-    echo $exception->getMessage();
-}
-///////
+require_once 'db.php';
 
 if (isset($_POST['title']) && isset($_POST['description']) && isset($_POST['url']))
 {
-
     $query = "INSERT INTO `article`(`title`, `description`, `url`) VALUES (:title, :description,:url)";
     $statement = $pdo->prepare($query);
     $statement->execute([
@@ -25,5 +12,5 @@ if (isset($_POST['title']) && isset($_POST['description']) && isset($_POST['url'
         'url' => $_POST['url']
     ]);
 
-    echo 'Votre article a bien été créé !';
+    header('Location: articles.php');
 }
